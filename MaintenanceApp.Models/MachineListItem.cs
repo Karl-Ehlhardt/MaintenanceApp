487b332d5a29f0864043a95c632aa8bde63737bb
sreskala <sam.sagas@hotmail.com>
2020-11-18 17:01:41 -0500

    Added Machine, machine create and mcahine edit

---
+++ MaintenanceApp.Models/MachineListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceApp.Models
+{
+    public class MachineListItem
+    {
+        public int MachineId { get; set; }
+        public string MachineName { get; set; }
+    }
+}
---
+++ MaintenanceApp.Services/MachineService.cs
@@ -0,0 +1,25 @@
+﻿using MaintenanceApp.Data.UserData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceApp.Services
+{
+    public class MachineService
+    {
+        //private user field
+        private readonly Guid _userId;
+
+        //private appcontext
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        //constructor
+        public MachineService(Guid userId)
+        {
+            userId = _userId;
+        }
+
+    }
+}
