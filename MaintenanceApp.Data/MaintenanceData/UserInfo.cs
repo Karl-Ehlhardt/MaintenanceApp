769ac77ebeda6605706811d7038171a4283d4fb0
David Sprinkle <davesprinklemusic@gmail.com>
2020-11-20 09:48:27 -0500

    Adding Users

---
+++ MaintenanceApp.Data/MaintenanceData/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaintenanceApp.Data.MaintenanceData
+{
+    public class UserInfo
+    {
+        [Key]
+        public int UserInfoId { get; set; }
+
+
+
+    }
+}
