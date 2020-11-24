aa096ff117357bb99c1c1951d5d0176e0928ca20
Karl Ehlhardt <ehlhardt_karl@gmail.com>
2020-11-24 18:47:03 -0500

    Karl/11

--- MaintenanceApp.Services/TasksForMachineService.cs
+++ MaintenanceApp.Services/TasksForMachineService.cs
@@ -1,5 +1,6 @@
 ﻿using MaintenanceApp.Data.MaintenanceData;
 using MaintenanceApp.Data.UserData;
+using MaintenanceApp.Models.Machine;
 using MaintenanceApp.Models.TasksForMachine;
 using System;
 using System.Collections.Generic;
@@ -164,6 +165,93 @@ namespace MaintenanceApp.Services
             return query;
         }
 
+        //[ActionName("GetAllActiveTasksThatAreUnassignedByIdForBuildingAreaOrMachine")]
+        public async Task<List<TasksForMachineByMachineIdLookup>> GetAllActiveTasksThatAreUnassignedByIdForBuildingAreaOrMachine([FromUri] int id, [FromBody] string search)
+        {
+            List<int> machineList = new List<int>();
+            switch (search)
+            {
+                case "Building":
+                    List<int> areaInBuildingList =
+                    _context
+                    .Areas
+                    .Where(a => a.BuildingId == id)
+                    .Select(
+                    a => a.AreaId).ToList();
+
+                    foreach (int areaId in areaInBuildingList)
+                    {
+                        List<int> machineInBuildingListStep =
+                                    _context
+                                    .Machines
+                                    .Where(m => m.AreaId == areaId)
+                                    .Select(
+                                        m => m.MachineId).ToList();
+                        foreach (int machineId in machineInBuildingListStep)
+                        {
+                            machineList.Add(machineId);
+                        }
+                    }
+                    break;
+
+                case "Area":
+                    List<int> machineInAreaListStep =
+                                _context
+                                .Machines
+                                .Where(m => m.AreaId == id)
+                                .Select(
+                                m => m.MachineId).ToList();
+                    foreach (int machineId in machineInAreaListStep)
+                    {
+                        machineList.Add(machineId);
+                    }
+                    break;
+
+                case "Machine":
+                    machineList.Add(id);
+                    break;
+
+                default:
+                    break;
+            }
+
+            List<TasksForMachineByMachineIdLookup> finalList = new List<TasksForMachineByMachineIdLookup>();
+
+            foreach (int machineId in machineList)
+            {
+                var queryMidStep =
+                    await
+                                _context.
+                                Machines.
+                                Where(m => m.MachineId == machineId).
+                                Select(m =>
+                                new TasksForMachineByMachineIdLookup
+                                {
+                                    MachineId = m.MachineId,
+                                    MachineName = m.MachineName,
+                                    TasksForMachineListItem = 
+                                        _context.
+                                        TasksForMachines.
+                                        Where(tm => tm.MachineId == m.MachineId && tm.Maintained == null && tm.ApplicationUserId == null).
+                                        Select(tm =>
+                                        new TasksForMachineListItem
+                                        {
+                                            Id = tm.MaintenanceTaskId,
+                                            MachineId = tm.MachineId,
+                                            Maintained = tm.Maintained,
+                                            NeedToBeMaintainedBy = tm.NeedToBeMaintainedBy,
+                                            MaintenanceTaskId = tm.MaintenanceTaskId,
+                                            ApplicationUserId = tm.ApplicationUserId
+
+                                        }).ToList()
+                                }).ToListAsync();
+                TasksForMachineByMachineIdLookup queryListItem = queryMidStep[0];
+                finalList.Add(queryListItem);
+            }
+
+            return finalList;
+        }
+
         //===========Update=============//
         public async Task<bool> CompleteAndGenerateNewTasksForMachineById([FromUri] int id)
         {
@@ -181,12 +269,12 @@ namespace MaintenanceApp.Services
                 .Single(t => t.MaintenanceTaskId == id);
 
             TasksForMachine newTaskMachine = new TasksForMachine()
-                    {
-                        MachineId = refrence.MachineId,
-                        NeedToBeMaintainedBy = new DateTimeOffset(DateTime.Now, refrence.MaintenanceTaskInterval),
-                        MaintenanceTaskId = refrence.MaintenanceTaskId,
-                        ApplicationUserId = refrence.ApplicationUserId
-                    };
+            {
+                MachineId = refrence.MachineId,
+                NeedToBeMaintainedBy = new DateTimeOffset(DateTime.Now, refrence.MaintenanceTaskInterval),
+                MaintenanceTaskId = refrence.MaintenanceTaskId,
+                ApplicationUserId = refrence.ApplicationUserId
+            };
 
             _context.TasksForMachines.Add(newTaskMachine);
 
