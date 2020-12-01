cf425ceaa273673ed0fdd745ed634b97f0104079
Karl Ehlhardt <ehlhardt_karl@gmail.com>
2020-12-01 16:05:11 -0500

    Oversite base files added

--- MaintenanceApp.Services/TasksForMachineService.cs
+++ MaintenanceApp.Services/TasksForMachineService.cs
@@ -102,7 +102,7 @@ namespace MaintenanceApp.Services
             //    }
             //}
 
-            return await _context.SaveChangesAsync() == 1;
+            return await _context.SaveChangesAsync() >= 1;
         }
 
         //=================READ====================//
@@ -357,7 +357,7 @@ namespace MaintenanceApp.Services
 
             _context.TasksForMachines.Add(newTaskMachine);
 
-            return await _context.SaveChangesAsync() == 1;
+            return await _context.SaveChangesAsync() >= 1;
         }
 
         //========Delete============//
